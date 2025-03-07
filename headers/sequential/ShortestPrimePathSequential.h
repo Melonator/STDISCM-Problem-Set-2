c807443c7699921d5a2483239b4b4d6857cdaf3e
Kovie <thekovie@gmail.com>
2025-03-07 14:47:16 +0800

    feat: add headers

--- headers/sequential/ShortestPrimePathSequential.h
+++ headers/sequential/ShortestPrimePathSequential.h
@@ -4,7 +4,16 @@
 
 class ShortestPrimePathSequential: public IPathBuilder {
 public:
-    void displayPath(const std::string &start, const std::string &end);
+    void displayPath(const std::string &start, const std::string &end, Graph *graph);
+
+private:
+    bool isPrime(int n);
+    struct State {
+        std::string node;
+        int weight;
+        std::vector<std::string> path;
+    };
+    struct StateComparator;
 };
 
 #endif //SHORTESTPRIMEPATHSEQUENTIAL_H
