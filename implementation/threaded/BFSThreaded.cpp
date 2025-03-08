7b1b2d6733b51a38891b04a59b41e4cecea7de00
Kovie <thekovie@gmail.com>
2025-03-08 15:10:36 +0800

    refactor: add placeholder function for JD's part

--- implementation/threaded/BFSThreaded.cpp
+++ implementation/threaded/BFSThreaded.cpp
@@ -1,5 +1,10 @@
 #include "../../headers/threaded/BFSThreaded.h"
+#include <iostream>
 /*
  * TODO: For JD
  *
  */
+
+void BFSThreaded::displayPath(const std::string &start, const std::string &end, Graph* graph) {
+    std::cout << "BFSThreaded displayPath not implemented.\n";
+}
--- implementation/threaded/ShortestPathThreaded.cpp
+++ implementation/threaded/ShortestPathThreaded.cpp
@@ -1,5 +1,10 @@
 #include "../../headers/threaded/ShortestPathThreaded.h"
+#include <iostream>
 /*
  * TODO: For JD
  *
  */
+
+void ShortestPathThreaded::displayPath(const std::string &start, const std::string &end, Graph* graph) {
+    std::cout << "ShortestPathThreaded displayPath not implemented.\n";
+}
