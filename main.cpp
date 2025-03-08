dca0d823f205c01c24663b6959b59fe1c234dc52
Kovie <thekovie@gmail.com>
2025-03-08 15:09:46 +0800

    feat: add threaded implementation in main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
+// Include both sequential and threaded headers.
 #include "headers/Graph.h"
 #include "headers/Pathfinder.h"
 #include "headers/ThreadPool.h"
+
+// Sequential implementations.
 #include "headers/sequential/ShortestPathSequential.h"
 #include "headers/sequential/BFSSequential.h"
 #include "headers/sequential/PrimePathSequential.h"
 #include "headers/sequential/ShortestPrimePathSequential.h"
+
+// Threaded implementations.
+#include "headers/threaded/ShortestPathThreaded.h"
+#include "headers/threaded/BFSThreaded.h"
+#include "headers/threaded/PrimePathThreaded.h"
+#include "headers/threaded/ShortestPrimePathThreaded.h"
 
 using namespace std;
 
 int main() {
     string file_name;
     string fullCommand;
+    // We'll hold our algorithm strategies via IPathBuilder pointers.
     std::unique_ptr<IPathBuilder> aStar;
     std::unique_ptr<IPathBuilder> bfs;
     std::unique_ptr<IPathBuilder> primePath;
@@ -20,29 +32,47 @@ int main() {
 
     int mode = 0;
     cout << "=== G R A P H  M A N I A ===" << "\n\n";
-    cout << "Enter the filename:";
+    cout << "Enter the filename: ";
     cin >> file_name;
-    Graph* graph;
-    Pathfinder pathFinder(graph);
-    cout << "[0] Serial Mode, [1] Parallel Mode:";
+
+    Graph* graph = nullptr;
+    // Create the Pathfinder using the graph pointer.
+    Pathfinder pathFinder(nullptr);
+
+    cout << "[0] Serial Mode, [1] Parallel Mode: ";
     cin >> mode;
 
     if (mode == 0) {
+        // Sequential mode.
         graph = new Graph(file_name);
         aStar = std::make_unique<ShortestPathSequential>();
         bfs = std::make_unique<BFSSequential>();
         primePath = std::make_unique<PrimePathSequential>();
         shortestPrimePath = std::make_unique<ShortestPrimePathSequential>();
     } else {
+        // Parallel mode.
         int numThreads = 0;
-        cout << "How many threads?";
+        cout << "How many threads? ";
         cin >> numThreads;
+        // Optionally, you can pass numThreads to your threaded implementations or store it globally.
+        graph = new Graph(file_name);
+        aStar = std::make_unique<ShortestPathThreaded>();
+        bfs = std::make_unique<BFSThreaded>();
+        primePath = std::make_unique<PrimePathThreaded>();
+        shortestPrimePath = std::make_unique<ShortestPrimePathThreaded>();
     }
 
+    // Update the Pathfinder with the created graph.
+    pathFinder = Pathfinder(graph);
+
     cin.ignore();
-    while (fullCommand != "exit") {
-        cout << "Enter a command:";
+    while (true) {
+        cout << "Enter a command: ";
         getline(cin, fullCommand);
+        if (fullCommand == "exit")
+            break;
+        
+        // Get the first token of the command.
         const string command = fullCommand.substr(0, fullCommand.find(' '));
 
         if (command == "node") {
@@ -55,37 +85,43 @@ int main() {
             const string node2 = fullCommand.substr(node1.length() + 6);
             const bool result = graph->hasEdge(node1, node2);
             const string output = result ? "The graph has edge " : "The graph DOES NOT have edge ";
-            cout << output << node1 << " "<< node2 << "\n";
+            cout << output << node1 << " " << node2 << "\n";
         } else if (command == "path") {
+            // Format: path node1 node2
             const string node1 = fullCommand.substr(5, fullCommand.find(' ', 5) - 5);
             const string node2 = fullCommand.substr(node1.length() + 6);
             pathFinder.setStrategy(bfs.get());
             pathFinder.displayPath(node1, node2, graph);
         } else if (command == "prime-path") {
+            // Format: prime-path node1 node2
             size_t firstSpace = fullCommand.find(' ');
             size_t secondSpace = fullCommand.find(' ', firstSpace + 1);
-
             string node1 = fullCommand.substr(firstSpace + 1, secondSpace - firstSpace - 1);
             string node2 = fullCommand.substr(secondSpace + 1);
             pathFinder.setStrategy(primePath.get());
             pathFinder.displayPath(node1, node2, graph);
         } else if (command == "shortest-prime-path") {
+            // Format: shortest-prime-path node1 node2
             size_t firstSpace = fullCommand.find(' ');
             size_t secondSpace = fullCommand.find(' ', firstSpace + 1);
-
             string node1 = fullCommand.substr(firstSpace + 1, secondSpace - firstSpace - 1);
             string node2 = fullCommand.substr(secondSpace + 1);
             pathFinder.setStrategy(shortestPrimePath.get());
             pathFinder.displayPath(node1, node2, graph);
         } else if (command == "shortest-path") {
+            // Format: shortest-path node1 node2
             size_t firstSpace = fullCommand.find(' ');
             size_t secondSpace = fullCommand.find(' ', firstSpace + 1);
-
             string node1 = fullCommand.substr(firstSpace + 1, secondSpace - firstSpace - 1);
             string node2 = fullCommand.substr(secondSpace + 1);
             pathFinder.setStrategy(aStar.get());
             pathFinder.displayPath(node1, node2, graph);
+        } else {
+            cout << "Invalid command.\n";
         }
     }
+
+    // Cleanup
+    delete graph;
     return 0;
 }
